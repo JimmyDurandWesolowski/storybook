1ea0cbf1cdd536880f5041138554c99764fbdee6
Jimmy Durand Wesolowski <jimmy.wesolowski@mobileye.com>
2024-05-29 18:38:05 +0200

    main: Add argument parser, use tempfile
    Signed-off-by: Jimmy Durand Wesolowski <jimmy.wesolowski@mobileye.com>

--- main.py
+++ main.py
@@ -4,14 +4,13 @@
 A program that generates storybook pages with Ollama and Stable Diffusion for the Inky Impression
 """
 
+import argparse
 import math
+import pathlib
 import requests
 import subprocess
-import time
+import tempfile
 from PIL import Image, ImageDraw, ImageFont
-from inky.auto import auto
-
-display = auto()
 
 GENERATION_INTERVAL = 1800 #seconds
 DISPLAY_RESOLUTION = (448, 600)
@@ -21,12 +20,9 @@ OLLAMA_MODEL = 'gemma:7b'
 OLLAMA_PROMPT = '''Create text from the page of an illustrated children\'s fantasy book. 
 This text should be around 20 words. If you desire, you can include a hero, monster, mythical 
 creature or artifact. You can choose a random mood or theme. Be creative.'''.replace("\n", "")
-SD_LOCATION = '/home/user/OnnxStream/src/build/sd'
-SD_MODEL_PATH = '/home/user/sd_models/stable-diffusion-xl-turbo-1.0-onnxstream'
 SD_PROMPT = 'an illustration in a children\'s book for the following scene: '
 SD_STEPS = 3
-TEMP_IMAGE_FILE = '/home/user/storybook/image.png' # for temp image storage
-FONT_FILE = '/home/user/storybook/CormorantGaramond-Regular.ttf'
+FONT_FILE = 'CormorantGaramond-Regular.ttf'
 FONT_SIZE = 21
 
 def get_story():
@@ -54,31 +50,51 @@ def split_text(text):
         text = replace_next_space_with_newline(text,approx_line_len*(i+1))
     return text
 
-def generate_page():
+def generate_page(args, page_idx):
     generated_text = get_story()
     print(f'text: {generated_text}')
-    subprocess.run([SD_LOCATION, '--xl', '--turbo', '--rpi', '--models-path', SD_MODEL_PATH,\
-                    '--prompt', SD_PROMPT+f'"{generated_text}"',\
-                    '--steps', f'{SD_STEPS}', '--output', TEMP_IMAGE_FILE], check=False) 
-    generated_text = split_text(generated_text)
-
-    canvas = Image.new(mode="RGB", size=DISPLAY_RESOLUTION, color="white")
-    im2 = Image.open(TEMP_IMAGE_FILE)
+    with tempfile.NamedTemporaryFile() as temp:
+        subprocess.run(
+            [
+                args.sd_path,
+                "--xl",
+                "--turbo",
+                "--rpi",
+                "--models-path",
+                args.sd_model,
+                "--prompt",
+                SD_PROMPT + f'"{generated_text}"',
+                "--steps",
+                f"{SD_STEPS}",
+                "--output",
+                temp.name,
+            ],
+            check=False,
+        )
+        generated_text = split_text(generated_text)
+        canvas = Image.new(mode="RGB", size=DISPLAY_RESOLUTION, color="white")
+        im2 = Image.open(temp.name)
     im2 = im2.resize((448,448))
     canvas.paste(im2)
     im3 = ImageDraw.Draw(canvas)
     font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
     im3.text((7, 450), generated_text, font=font, fill=(0, 0, 0))
     canvas.show()
-    canvas.save('output.png') # save a local copy for closer inspection
+    canvas.save(f'output-{page_idx}.png')
     canvas = canvas.rotate(90,expand=1)
-    display.set_image(canvas)
-    display.show()
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--onnxstream", "--onnxstream-path", required=True, type=pathlib.Path
+    )
+    parser.add_argument("--sd-path", required=True, type=pathlib.Path)
+    parser.add_argument("--sd-model", required=True, type=pathlib.Path)
+    args = parser.parse_args()
+    _idx = 1
     while True:
-        generate_page()
-        time.sleep(GENERATION_INTERVAL)
+        generate_page(args, _idx)
+        _idx += 1
 
 if __name__ == '__main__':
     main()
